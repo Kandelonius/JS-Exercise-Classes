b6d2a8e9c45e76e44dcc2e5626c38d3d375dbf03
Shane Kaestner <shane.kaestner@yahoo.com>
2020-03-26 16:59:42 -0700

    finished all stretch goals as well.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
     - Write an Airplane class whose constructor initializes `name` from an argument.
     - All airplanes built with Airplane should initialize with an `isFlying` property of false.
     - Give airplanes the ability to `.takeOff()` and `.land()`:
-        + If a plane takes off, its `isFlying` property gets set to true.
-        + If a plane lands, its `isFlying` property gets set to false.
+      + If a plane takes off, its `isFlying` property gets set to true.
+      + If a plane lands, its `isFlying` property gets set to false.
 */
 
 // EXAMPLE SOLUTION CODE:
@@ -32,12 +32,12 @@ class Airplane {
     - Write a Person class whose constructor initializes `name` and `age` from arguments.
     - All instances of Person should also initialize with an empty `stomach` array.
     - Give instances of Person the ability to `.eat("someFood")`:
-        + When eating an edible, it should be pushed into the `stomach`.
-        + The `eat` method should have no effect if there are 10 items in the `stomach`.
+      + When eating an edible, it should be pushed into the `stomach`.
+      + The `eat` method should have no effect if there are 10 items in the `stomach`.
     - Give instances of Person the ability to `.poop()`:
-        + When an instance poops, its `stomach` should empty.
+      + When an instance poops, its `stomach` should empty.
     - Give instances of Person a method `.toString()`:
-        + It should return a string with `name` and `age`. Example: "Mary, 50"
+      + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 // function Person(name, age) {
 //   this.name = name;
@@ -78,14 +78,14 @@ class Person {
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
     - All instances built with Car:
-        + should initialize with a `tank` at 0
-        + should initialize with an `odometer` at 0
+      + should initialize with a `tank` at 0
+      + should initialize with an `odometer` at 0
     - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
     - Give cars ability to `.drive(distance)`. The distance driven:
-        + Should cause the `odometer` to go up.
-        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+      + Should cause the `odometer` to go up.
+      + Should cause the the `tank` to go down taking `milesPerGallon` into account.
     - A car which runs out of `fuel` while driving can't drive any more distance:
-        + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+      + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 // function Car(model, milesPerGallon) {
 //   this.model = model;
@@ -144,13 +144,13 @@ class Car {
   TASK 3
     - Write a Lambdasian class.
     - Its constructor takes a single argument - an object with the following keys:
-        + name
-        + age
-        + location
+      + name
+      + age
+      + location
     - Its constructor should initialize `name`, `age` and `location` properties on the instance.
     - Instances of Lambdasian should be able to `.speak()`:
-        + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
-        + {name} and {location} of course come from the instance's own properties.
+      + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
+      + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
   constructor(args){
@@ -167,15 +167,15 @@ class Lambdasian {
   TASK 4
     - Write an Instructor class extending Lambdasian.
     - Its constructor takes a single argument - an object with the following keys:
-        + All the keys used to initialize instances of Lambdasian.
-        + `specialty`: what the instance of Instructor is good at, i.e. 'redux'
-        + `favLanguage`: i.e. 'JavaScript, Python, Elm etc.'
-        + `catchPhrase`: i.e. `Don't forget the homies`.
+      + All the keys used to initialize instances of Lambdasian.
+      + `specialty`: what the instance of Instructor is good at, i.e. 'redux'
+      + `favLanguage`: i.e. 'JavaScript, Python, Elm etc.'
+      + `catchPhrase`: i.e. `Don't forget the homies`.
     - The constructor calls the parent constructor passing it what it needs.
     - The constructor should also initialize `specialty`, `favLanguage` and `catchPhrase` properties on the instance.
     - Instructor instances have the following methods:
-        + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
-        + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
+      + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+      + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
     STRETCH PROBLEM (no tests!)
       - Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
 */
@@ -213,18 +213,21 @@ class Instructor extends Lambdasian{
   TASK 5
     - Write a Student class extending Lambdasian.
     - Its constructor takes a single argument -  an object with the following keys:
-        + All the keys used to initialize instances of Lambdasian.
-        + `previousBackground` i.e. what the Student used to do before Lambda School
-        + `className` i.e. CS132
-        + `favSubjects`. i.e. an array of the student's favorite subjects ['HTML', 'CSS', 'JS']
+      + All the keys used to initialize instances of Lambdasian.
+      + `previousBackground` i.e. what the Student used to do before Lambda School
+      + `className` i.e. CS132
+      + `favSubjects`. i.e. an array of the student's favorite subjects ['HTML', 'CSS', 'JS']
     - The constructor calls the parent constructor passing to it what it needs.
     - The constructor should also initialize `previousBackground`, `className` and `favSubjects` properties on the instance.
     - Student instances have the following methods:
-        + `listSubjects` a method that returns all of the student's favSubjects in a single string: `Loving HTML, CSS, JS!`.
-        + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
-        + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
+      + `listSubjects` a method that returns all of the student's favSubjects in a single string: `Loving HTML, CSS, JS!`.
+      + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
+      + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   STRETCH PROBLEM (no tests!)
     - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
+    - Add a graduate method to a student.
+      + This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
+      + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
 */
 class Student extends Lambdasian{
   constructor(args){
@@ -249,20 +252,27 @@ class Student extends Lambdasian{
   stateGrade(){
     return `My grade is ${this.grade}!`;
   }
+  graduate(){
+    if (this.grade >= 70){
+      return `${this.name} is ready to graduate with a grade of ${this.grade}... wait thet\'s me woohoo!!!`;
+    } else {
+      return `The grind is real I only have ${this.grade} so I must continue.`;
+    }
+  }
 }
 
 /*
   TASK 6
     - Write a ProjectManager class extending Instructor.
     - Its constructor takes a single argument - an object with the following keys:
-        + All the keys used to initialize instances of Instructor.
-        + `gradClassName`: i.e. CS1
-        + `favInstructor`: i.e. Sean
+      + All the keys used to initialize instances of Instructor.
+      + `gradClassName`: i.e. CS1
+      + `favInstructor`: i.e. Sean
     - Its constructor calls the parent constructor passing to it what it needs.
     - The constructor should also initialize `gradClassName` and `favInstructor` properties on the instance.
     - ProjectManager instances have the following methods:
-        + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
-        + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
+      + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
+      + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
     STRETCH PROBLEM (no tests!)
       - Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
 */
@@ -289,7 +299,6 @@ class ProjectManager extends Instructor{
       + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
 */
 
-
 const passingStudent = new Student({
   name : 'James',
   age : 23,
@@ -299,7 +308,15 @@ const passingStudent = new Student({
   favSubjects : ['Python', 'Statistics', 'Newtonian Physics'], 
   grade : 85
 })
-
+const studentNotPassing = new Student({
+  name: 'Matt',
+  age: 30,
+  location: 'London',
+  previousBackground: 'Plumber',
+  className: 'WebEU 3',
+  favSubjects: ['JS', 'Node', 'Redux'],
+  grade : 64
+})
 const awesoneTeacher = new Instructor({
   name : 'Cassidy',
   age : 41,
@@ -319,6 +336,9 @@ const awesonePM = new ProjectManager({
   favInstructor: 'Cassidy'
 })
 console.log(passingStudent.stateGrade());
+console.log(studentNotPassing.stateGrade());
+console.log(passingStudent.graduate());
+console.log(studentNotPassing.graduate());
 console.log(awesoneTeacher.studentPerformance(passingStudent));
 console.log(awesonePM.studentPerformance(passingStudent));
 
